cd898d926f28e07931d31327eef43247005f89f7
MRIGL <135025203+MRIGL@users.noreply.github.com>
2026-08-26 17:16:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Variable to count POST requests
 let postCount = 0;
 
